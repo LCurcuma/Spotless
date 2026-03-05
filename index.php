2c1517671ad0f4b20ca6d2842ebe27176a5bdb80
LCurcuma <zhihnadya2002@gmail.com>
2026-03-05 16:02:42 +0100

    edited styles + added functions for pop-ups

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
  */
 
 require "settings/init.php";
+
+date_default_timezone_set('Europe/Copenhagen');
+$date = date("Y-m-d H:i:s");
+$today = date("d.m.Y");
+$day = date("Y-m-d");
+$timeNow = date("H:i");
+
+$sqlHistory = "SELECT * FROM History ORDER BY date DESC";
+$history = $db->sql($sqlHistory);
+$firstDate = $history[0]->date;
+$firstDato = date("d.m.Y", strtotime($firstDate));
+$firstTime = date("H:i", strtotime($firstDate));
 ?>
 <!DOCTYPE html>
 <html lang="da">
@@ -23,58 +35,117 @@ require "settings/init.php";
 </head>
 
 <body>
+    <i class="fa-solid fa-user userIcon" style="color: rgb(255, 255, 255);" data-bs-target="#loginModal" data-bs-toggle="modal"></i>
 
-    <img src="img/logo.png" class="logoimg">
+<nav>
+<img src="img/logo.png" class="logoimg">
 
 <div class="text-end">
     <a href="">
         <button class="btn btn-primary kontaktknap"> KONTAKT </button>
     </a>
 </div>
-
+</nav>
 <div class="toilet">
     <div class="toiletrengjort text-center text-light">
         <p class="teksttoilet">Dette toilet blev rengjort</p>
         <div class="tid">
             kl.
-            <span id="cleanTime">--:--</span>
+            <span id="cleanTime"><?php echo $firstTime?></span>
         </div>
         <div class="dato" id="cleanDate">
-            --.--.----
+            <?php echo $firstDato?>
         </div>
     </div>
 </div>
 
 
-<div class="container">
-    <div class="cirkel1"></div>
-    <div class="cirkel2"></div>
-</div>
+    <div class="modal fade" id="loginModal" aria-hidden="true" aria-labelledby="loginModal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <label for="login" class="modal-title fs-5" id="loginLabel">Login</label>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <input class="form-control" type="password" id="login">
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" data-bs-target="#insertForm" data-bs-toggle="modal">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <i class="fa-solid fa-user userIcon" style="color: rgb(255, 255, 255);"></i>
+    <form action="index.php" method="post">
+        <div class="modal fade" id="insertForm" aria-hidden="true" aria-labelledby="insertForm" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="medarbejder">Medarbejder</label>
+                            <input type="text" class="form-control" id="medarbejder" name="medarbejder" value="Maria Hansen">
+                        </div>
+                        <div class="mb-3 d-flex flex-row">
+                            <div class="mb-3">
+                                <label for="date">Date</label>
+                                <input type="date" class="form-control" id="date" name="date" value="<?php echo $today; ?>">
+                            </div>
+                            <div class="mb-3 ms-auto">
+                                <label for="tidspunkt">Tidspunkt</label>
+                                <input type="time" class="form-control" id="tidspunkt" name="tidspunkt" value="<?php echo $timeNow; ?>">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="andet">Evt. bemærkninger</label>
+                            <input type="text" class="form-control" id="andet" name="andet">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-primary" data-bs-target="#inertForm" data-bs-dismiss="modal" id="saveCleanBtn">INDSEND</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 
-<div class="background">
-    <img src="img/background-abstract.png" class="backgroundimg">
-    <img src="img/background-abstract.png" class="backgroundimg2">
-</div>
+    <?php
+        $sqlWorkers = "SELECT * FROM Workers";
+        $workers = $db->sql($sqlWorkers);
+
+        if(!empty($_POST)){
+            $sqlInsert = "INSERT INTO History (workerId, toiletId, date) VALUES (:workerId, :toiletId, :date)";
+            $workerID = null;
+
+            foreach($workers as $worker){
+                if($worker->name===$_POST['medarbejder']){
+                    $workerID = $worker->id;
+                }
+            }
+            $bindsInsert = [
+                    ":workerId" => $workerID,
+                ":toiletId" => "2",
+                ":date" => $_POST['date']." ".$_POST['tidspunkt']
+            ];
+            $db->sql($sqlInsert, $bindsInsert);
+            echo "<div class='bg-light d-flex flex-column justify-content-center align-items-center goodJob'><p>Godt arbejde!</p><a href='index.php' class='btn btn-primary'>Tak :)</a></div>";
+        }
+    ?>
+
+
+    <div class="container">
+        <div class="cirkel1"></div>
+        <div class="cirkel2"></div>
+    </div>
+
+    <div class="background">
+        <img src="img/background-abstract.png" class="backgroundimg">
+        <img src="img/background-abstract.png" class="backgroundimg2">
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-    /*
-    const cleanTime = document.querySelector('#cleanTime');
-    const cleanDate = document.querySelector('#cleanDate');
-
-    const saveCleanBtn = document.querySelector('#saveCleanBtn');
-    saveCleanBtn.addEventListener('click', () => {
-
-        const cleanTimeInput = document.querySelector('#cleanTimeInput');
-        const cleanDateInput = document.querySelector('#cleanDateInput');
-        cleanTime.innerHTML = cleanTimeInput.value;
-        cleanDate.innerHTML = cleanDateInput.value;
-
-    })
-
-*/
-</script>
 </body>
 </html>
