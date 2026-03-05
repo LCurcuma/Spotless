57bbd72b84a7ae36f300e901d121f872186ee317
LCurcuma <zhihnadya2002@gmail.com>
2026-03-05 12:52:39 +0100

    edited styles + added button-icon

--- index.php
+++ index.php
@@ -19,25 +19,18 @@ require "settings/init.php";
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/737b386bab.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
-<div class="background">
-    <img src="img/background-abstract.png" class="backgroundimg">
-    <img src="img/background-abstract.png" class="backgroundimg2">
-</div>
-
-<div class="logo">
     <img src="img/logo.png" class="logoimg">
-</div>
 
 <div class="text-end">
     <a href="">
         <button class="btn btn-primary kontaktknap"> KONTAKT </button>
     </a>
 </div>
-
 
 <div class="toilet">
     <div class="toiletrengjort text-center text-light">
@@ -58,21 +51,20 @@ require "settings/init.php";
     <div class="cirkel2"></div>
 </div>
 
+    <i class="fa-solid fa-user userIcon" style="color: rgb(255, 255, 255);"></i>
 
-
-
-
-
-
-
-
+<div class="background">
+    <img src="img/background-abstract.png" class="backgroundimg">
+    <img src="img/background-abstract.png" class="backgroundimg2">
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
+    /*
     const cleanTime = document.querySelector('#cleanTime');
     const cleanDate = document.querySelector('#cleanDate');
 
-
+    const saveCleanBtn = document.querySelector('#saveCleanBtn');
     saveCleanBtn.addEventListener('click', () => {
 
         const cleanTimeInput = document.querySelector('#cleanTimeInput');
@@ -80,9 +72,9 @@ require "settings/init.php";
         cleanTime.innerHTML = cleanTimeInput.value;
         cleanDate.innerHTML = cleanDateInput.value;
 
-    }
+    })
 
-
+*/
 </script>
 </body>
 </html>
